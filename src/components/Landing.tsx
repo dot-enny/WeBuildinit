9ea53cbcd6c4aab30e6a866ca5e8b579bb7737db
dot-enny <olaniyaneniola0225@gmail.com>
2025-02-03 17:18:47 +0100

    chore: removed unused code.

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -7,16 +7,14 @@ const Landing = () => {
 
 
   return (
-    // <div className="">
-      <div className="bg-[#222222]">
-        <Hero />
-        <div className="max-w-[1440px] md:w-[85%] max-sm:px-4 mx-auto">
-          <Benefits />
-          <Faq />
-          <Footer />
-        </div>
+    <div className="bg-[#222222]">
+      <Hero />
+      <div className="max-w-[1440px] md:w-[85%] max-sm:px-4 mx-auto">
+        <Benefits />
+        <Faq />
+        <Footer />
       </div>
-    // </div>
+    </div>
   );
 };
 
