0bce69cc77feff05bfb0a25205dc883dc607dfd0
dot-enny <olaniyaneniola0225@gmail.com>
2025-02-07 17:52:40 +0100

    hot-fix: app kit crash force auth.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Landing from './components/Landing';
 import "./App.css";
 import { ConnectWalletProvider } from './context/ConnectWalletContext';
 import SidebarLayout from './components/sidebar/SidebarLayout';
 import { Tasks } from './pages/Tasks';
 
-import { createAppKit } from '@reown/appkit/react'
+// import { createAppKit } from '@reown/appkit/react'
 import { SolanaAdapter } from '@reown/appkit-adapter-solana/react'
-import { solana, solanaTestnet, solanaDevnet } from '@reown/appkit/networks'
+// import { solana, solanaTestnet, solanaDevnet } from '@reown/appkit/networks'
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { useAppStateStore } from './lib/AppStateStore';
+// import { useEffect } from 'react';
+
 
 // 0. Set up Solana Adapter
 const solanaWeb3JsAdapter = new SolanaAdapter({
@@ -27,60 +31,63 @@ const metadata = {
 }
 
 // 3. Create modal
-createAppKit({
-  adapters: [solanaWeb3JsAdapter],
-  networks: [solana, solanaTestnet, solanaDevnet],
-  metadata: metadata,
-  projectId,
-  features: {
-    analytics: true // Optional - defaults to your Cloud configuration
-  }
-})
+const walletAddress = sessionStorage.getItem('walletAddress');
+if(walletAddress) {
+  console.log(walletAddress)
+} else {
+  console.log('no wallet address')
+}
+// if (!walletAddress) {
+//   createAppKit({
+//     adapters: [solanaWeb3JsAdapter],
+//     networks: [solana, solanaTestnet, solanaDevnet],
+//     metadata: metadata,
+//     projectId,
+//     features: {
+//       analytics: true // Optional - defaults to your Cloud configuration
+//     }
+//   })
+// }
+
+const queryClient = new QueryClient()
 
 function App() {
 
-
+  // const { walletAddress } = useAppStateStore();
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <ConnectWalletProvider>
-                <Landing />
-              </ConnectWalletProvider>
-            </div>
-          }
-        />
-        {/* <Route
-              path="/chat"
-              element={
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <ConnectWalletProvider>
+                  {/* {walletAddress ? ( // Conditionally render based on walletAddress
+                    <Navigate to="/tasks" replace={true} /> // Redirect if walletAddress exists
+                  ) : ( */}
+                    <Landing /> // Render Landing component if walletAddress is null/undefined
+                  {/* )} */}
+                </ConnectWalletProvider>
+              </div>
+            }
+          />
+          <Route
+            path="/tasks"
+            element={
+              <div>
                 <SidebarLayout>
-                  <Chat />
+                  <Tasks />
                 </SidebarLayout>
-              }
-            /> */}
-        <Route
-          path="/tasks"
-          element={
-            <div>
-              {/* <WalletProvider> */}
+              </div>
+            }
+          />
+          {/* New page */}
 
-                <SidebarLayout>
-                  {/* <FileProvider> */}
-                    <Tasks />
-                  {/* </FileProvider> */}
-                </SidebarLayout>
-              {/* </WalletProvider> */}
-            </div>
-          }
-        />
-        {/* New page */}
-
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
